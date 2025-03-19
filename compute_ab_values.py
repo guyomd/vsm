68e26f0a365788ccbd678f6d679f4c23c58bdc4b
guyomd <guillaume.daniel@outlook.com>
2025-03-19 12:05:48 +0100

    removed code and modules for the penalized likelihood estimation method

--- compute_ab_values.py
+++ compute_ab_values.py
@@ -12,7 +12,6 @@ from lib.ioutils import (ParameterSet,
                          load_grid,
                          load_fmd_file)
 from lib.grt_ml import Dutfoy2020_Estimator
-from lib.grt_pl import penalized_likelihood, load_mcmc_results
 
 COORD_PRECISION = 1E-6  # Precision used for the comparison of cell coordinates
 
@@ -187,106 +186,8 @@ class TruncatedGRestimator():
             stda = np.sqrt(cov[1, 1])
             self.grt_params[i, :] = np.array([lon, lat, a, b, stda, stdb, rho, mc])
 
-    def _PL_estimation(self, max_mcmc_trials=1E6, global_b_prior_mean=1.0, global_b_prior_std=0.3,
-                       apply_smoothing=True, b_truncation=None):
-        """
-        Penalized-likelihood estimation of (a, b) parameters (EPRI, 2012).
-        This algorithm inverts jointly paramters for all cells of the domain,
-        using a MCMC algorithm. Thus, convergence can be long...
-        """
-        raise Warning('Estimation of FMD parameters using the Penalized Likelihood (PL) approach is still experimental...')
-        # First, remove cells with null density in the first bin
-        # (indicating null density in every bins):
-        condition = (self.densities[:, 2] != 0.0) & (self.prior_b[:, 0] != -9.0)
-        bin_durations, cellinfo, densities, prior_b , i_kept, i_removed = \
-            self._select_cells(condition)
-        m = cellinfo.shape[0]  # Number of cells with density > 0
 
-        areas = np.ones((m, ))
-        imc = np.where(self.bins['mins'] >= self.mmin)[0][0]
-        bins = np.array([self.bins['mins'], self.bins['maxs']]).T
-        nb = bins.shape[0]
-        bins_w = np.ones((nb,))  # Can be changed to favor lower-/higher-magntiude bins
-        m0 = self.mmin
-
-        # Build a PMF composed of a single MMAX value:
-        mmax_pmf = np.ones((m, 1, 2))
-        for k in range(m):
-            mmax_pmf[k, 0, :] = np.array([cellinfo[k, 3], 1.0])
-
-        # Define prior on parameters:
-        prior = dict()
-        prior['b_mean'] = global_b_prior_mean
-        prior['b_std'] = global_b_prior_std
-        lograte_m0 = np.log10(densities[:, 2])
-        beta_prior = prior_b[:, 0] * np.log(10)
-        beta_std_prior = prior_b[:, 1] * np.log(10)
-        prior['mins'] = np.array((lograte_m0 - 2.0).tolist() + (beta_prior - 2 * beta_std_prior).tolist() + [0.1, 0.1])
-        prior['maxs'] = np.array((lograte_m0 + 2.0).tolist() + (beta_prior + 2 * beta_std_prior).tolist() + [1, 4])
-        if b_truncation is not None:
-            # Apply truncation to beta values if needed:
-            prior['mins'][m:(2 *m)] = np.fmin(prior['mins'][m:(2 * m)], b_truncation[0] * np.log(10) * np.ones((m,)))
-            prior['maxs'][m:(2 * m)] = np.fmax(prior['maxs'][m:(2 * m)], b_truncation[1] * np.log(10) * np.ones((m,)))
-
-        n_warmup = round(max_mcmc_trials * 0.01)
-        penalized_likelihood(densities[:, (2 + imc):],
-                             areas,
-                             cellinfo[:, 0],
-                             cellinfo[:, 1],
-                             bins[imc:, :],
-                             bin_durations[:, imc:],
-                             mmax_pmf,
-                             bins_w[imc],
-                             m0=m0,
-                             max_trials=max_mcmc_trials,
-                             n_init=n_warmup,
-                             collection_interval='auto',
-                             prior=prior,
-                             proposal={'fun_type': 'uniform-local', 'sigma': 0.1, 'strategy': 'incremental'},
-                             output_file='mcmc_samples.out',
-                             only_sample_prior=True,
-                             seed=None,
-                             apply_smoothing=apply_smoothing)
-        lons, lats, index, post, accept, a, b, sm_beta, sm_lograte = \
-            load_mcmc_results(file="mcmc_samples.out",
-                              subsampling_step=1,
-                              grid_reshape=None,
-                              additive_scaling_for_a=0.0)
-
-        # Compute summary statistics:
-        sm_beta_mean = np.mean(sm_beta)
-        sm_beta_std = np.std(sm_beta)
-        sm_lograte_mean = np.mean(sm_lograte)
-        sm_lograte_std = np.std(sm_lograte)
-        print('# Optimal smoothing control parameters:')
-        print(f'  --> for beta: {sm_beta_mean} +/- {sm_beta_std}')
-        print(f'  --> for log10(rate): {sm_lograte_mean} +/- {sm_lograte_std}')
-
-        a_mean = np.mean(a, axis=0)
-        a_std = np.mean(a, axis=0)
-        b_mean = np.mean(b, axis=0)
-        b_std = np.mean(b, axis=0)
-        rho = np.array([np.corrcoef(a[:, i], b[:, i], rowvar=True)[0, 1] for i in range(m)])
-        for j in range(m):
-            self.grt_params[i_kept[j], :] = np.array([lons[j],
-                                                      lats[j],
-                                                      a_mean[j],
-                                                      b_mean[j],
-                                                      a_std[j],
-                                                      b_std[j],
-                                                      rho[j],
-                                                      m0])
-        for j in range(len(i_removed)):
-            self.grt_params[i_removed[j], :] = np.array([self.densities[i_removed[j], 0],
-                                                         self.densities[i_removed[j], 1],
-                                                         np.nan,
-                                                         np.nan,
-                                                         np.nan,
-                                                         np.nan,
-                                                         np.nan,
-                                                         np.nan])
-
-    def run(self, method, options, print_warnings=False, b_truncation=None):
+    def run(self, options, print_warnings=False, b_truncation=None):
         """
         Evaluate a and b parameters of the truncated GR relationship in each cell
         """
@@ -294,19 +195,10 @@ class TruncatedGRestimator():
         if print_warnings:
             print(f'Warning !! Constant width is assumed for FMD bins in each cell (but can vary cell-wise)')
 
-        if method == 'ML':
-            self._ML_estimation(skip_missing_priors=options['skip_missing_priors'],
-                                auto_mc=options['auto_mc'],
-                                b_truncation=b_truncation)
-        elif method == 'PL':
-            self._PL_estimation(max_mcmc_trials=options['max_trials'],
-                                global_b_prior_mean=options['global_b_mean'],
-                                global_b_prior_std=options['global_b_std'],
-                                b_truncation=b_truncation)
-        else:
-            raise ValueError(f'Unrecognized method: "{method}"')
+        self._ML_estimation(skip_missing_priors=options['skip_missing_priors'],
+                            auto_mc=options['auto_mc'],
+                            b_truncation=b_truncation)
 
-    
     def write_to_csv(self, filename):
         """
         Save results in CSV format
@@ -361,25 +253,10 @@ if __name__ == "__main__":
                         nargs=2,
                         type=float)
 
-    parser.add_argument("-m", "--method",
-                        help="Method for estimation of MFD parameters (a, b): "+
-                             "'ML' for maximum-likelihood (Weichert, 1980), "+
-                             "'PL' for penalized-likelihood (EPRI, 2012)",
-                        choices=['ML', 'PL'],
-                        default='ML',
-                        type=str)
-
-    parser.add_argument('--par',
-                        help='Parameters for the penalized-likelihood approach: N_trials, Global priors on b (mean, std. dev.)',
-                        default=[1E6, 1.0, 0.3],
-                        nargs=3,
-                        type=float)
-
     parser.add_argument("--mmin",
                         help="Set the minimum magnitude used to estimate FMD parameters",
                         default=None,
                         type=float)
-    # TODO: Remove priors from --par option --> Keep only --b-prior option and make b-prior option mandatory when --par is used
     args = parser.parse_args()
     
     # Load parameters:
@@ -409,24 +286,12 @@ if __name__ == "__main__":
         print('>> No prior for b-values over the domain')
         estim.file_prior_b = None
 
-    if (args.b_prior is not None) and (args.method == 'PL'):
-        # In this case, option "b_prior" supercedes values in "pl-parameters":
-        args.par[1] = args.b_prior[0]
-        args.par[2] = args.b_prior[1]
-
-    # Set options for the chosen method:
-    if args.method == 'PL':
-        opts = {'max_trials': args.par[0],
-                'global_b_mean': args.par[1],
-                'global_b_std': args.par[2]
-                }
-    elif args.method == 'ML':
-        opts = {'skip_missing_priors': prms.skip_ab_if_missing_priors,
-                'auto_mc': prms.is_mc_automatic}
+    # Set options:
+    opts = {'skip_missing_priors': prms.skip_ab_if_missing_priors,
+           'auto_mc': prms.is_mc_automatic}
 
     # Estimate G-R parameters over cells:
-    estim.run(args.method,
-              opts,
+    estim.run(opts,
               print_warnings=False,
               b_truncation=args.b_truncation)
     
