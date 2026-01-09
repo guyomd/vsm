update formulas for the log-likelihood with fixed missing term in eqn.10

lib/grt_ml.py
@@ -13,7 +13,7 @@ Periods and Imprecise Magnitudes, Pure App. Geophysics, 177,
 10, 4597-4606, doi:10.1007/s00024-020-02551-8.
 """
 import numpy as np
-from scipy.optimize import minimize_scalar
+from scipy.optimize import minimize_scalar, root_scalar
 from scipy.special import gammaln
 
 